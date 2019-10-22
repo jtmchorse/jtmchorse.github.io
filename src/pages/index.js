bf3f840435a6f07cd15ab4ac78e732a520eab45d
JT McHorse <jmchorse@gmail.com>
2019-10-22 06:15:04 -0700

    merged in day-8 branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,86 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div>
+      <h2>Day 8</h2>
+      <p>
+        Gatsby Tutorial part-two, css. Specifically{" "}
+        <a
+          href="https://www.gatsbyjs.org/tutorial/part-two/#using-component-scoped-css"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          #using-component-scoped-css
+        </a>
+      </p>
+      <p>
+        This example takes us through building a micro component, and giving it
+        some css. Not just any css file though, this is a .module.css file
+      </p>
+      <p>Why is that special?</p>
+      <p>Component-scoped CSS</p>
+      <p>
+        As the tutorial page describes, "CSS Modules are very popular because
+        they let you write CSS normally but with a lot more safety. The tool
+        automatically generates unique class and animation names, so you don’t
+        have to worry about selector name collisions."
+      </p>
+      <p>But why is that useful?</p>
+      <p>
+        Lets take a look at our{" "}
+        <Link to="/about-css-modules/">container componet</Link> that we created
+        in this section.
+      </p>
+      <img
+        src={"../../Screenshot from 2019-10-22 05-44-17.png"}
+        alt="Screen shot of css component"
+      />
+      <p>
+        Nothing super fancy, obviously. But we can still use this to try and
+        learn what we should be here.
+      </p>
+      <img
+        src={"../../Screenshot from 2019-10-22 06-06-11.png"}
+        alt="Browser rendered view of css component"
+      />
+      <p>
+        What is super neat here in the inspector is that we have unique
+        classnames for the component. According to the tutorial, by using the
+        css as a .module.css Gatsby will render components like this, all
+        bundled up as one little self contained thing. All so we don't have css issues with other
+        components. Thanks Obama.
+      </p>
+      <p>
+        This is just a simple example of course, I'm assuming that in the future
+        you can hold components inside other components and so on. Feels like
+        building any sort of not basic site would require that.
+      </p>
+      <p>Unexpected issues from today</p>
+      <ul>
+        <li>Inserting images</li>
+      </ul>
+
+    </div>
+    <div>
       <h2>Day 7</h2>
       <p>package.json</p>
-      <p>Long story short, as best I can tell running the gatsby-cli update installed a newer dependency inside my repo, and there was the mismatch between gatsby-cli and react. Finally I side by side checked both pages in my fancy site vs a fresh install and aside from the gh-deploy script, the only difference I could find was that cli update in the dependencies</p>
-      <p>How to fix it? Delete that gatsby-cli line in the package.json, trash the node-modules folder and re-run yarn</p>
+      <p>
+        Long story short, as best I can tell running the gatsby-cli update
+        installed a newer dependency inside my repo, and there was the mismatch
+        between gatsby-cli and react. Finally I side by side checked both pages
+        in my fancy site vs a fresh install and aside from the gh-deploy script,
+        the only difference I could find was that cli update in the dependencies
+      </p>
+      <p>
+        How to fix it? Delete that gatsby-cli line in the package.json, trash
+        the node-modules folder and re-run yarn
+      </p>
       <p>Tentatively run gatsby develop...</p>
       <p>wait</p>
       <p>Error! WHAT!... Oh..</p>
-      <p>Something is on port 8000 already, go ahead and use a different one? Ya, I think I will</p>
+      <p>
+        Something is on port 8000 already, go ahead and use a different one? Ya,
+        I think I will
+      </p>
     </div>
     <div>
       <h2>Day 6</h2>
