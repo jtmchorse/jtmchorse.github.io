be0a2b34faa61311f71d46b9509255500aa8bfd3
JT McHorse <jmchorse@gmail.com>
2019-10-25 06:20:07 -0700

    Squashed commit of the following: - Day 11 feature branch
    commit cf7a308682d679840c25a51e955cddef1a1b67fd
    Author: JT McHorse <jmchorse@gmail.com>
    Date:   Fri Oct 25 06:17:07 2019 -0700
    
        Forgot some things.
    
    commit 26e52d05bdf5dc74438991842e467cce0811d31e
    Author: JT McHorse <jmchorse@gmail.com>
    Date:   Fri Oct 25 06:16:04 2019 -0700
    
        Day 11, plugins and new components/layouts.

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/home/jt/Documents/Gatsby/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-6153936-21"},
+    },{
       plugin: require('/home/jt/Documents/Gatsby/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
     }]
