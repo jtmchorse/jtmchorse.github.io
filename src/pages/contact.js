cf7a308682d679840c25a51e955cddef1a1b67fd
JT McHorse <jmchorse@gmail.com>
2019-10-25 06:17:07 -0700

    Forgot some things.

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout2';
+
+export default () => (
+    <Layout>
+
+        <div>
+            <h1>Hi, this is an example contact page.</h1>
+            <p>You can reach out to me on twitter, <a href="https://twitter.com/jmchorse">@jmchorse</a></p>
+        </div>
+
+    </Layout>
+
+)
