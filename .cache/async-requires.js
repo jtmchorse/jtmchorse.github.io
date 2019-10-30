97af5d8eb5d1f9e93f10e4231145418dc6000387
JT McHorse <jmchorse@gmail.com>
2019-10-30 06:09:46 -0700

    merged in day 15 branch

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,6 +6,7 @@ exports.components = {
   "component---src-pages-about-css-modules-js": () => import("/home/jt/Documents/Gatsby/src/pages/about-css-modules.js" /* webpackChunkName: "component---src-pages-about-css-modules-js" */),
   "component---src-pages-about-js": () => import("/home/jt/Documents/Gatsby/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-contact-js": () => import("/home/jt/Documents/Gatsby/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-index-js": () => import("/home/jt/Documents/Gatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---src-pages-index-js": () => import("/home/jt/Documents/Gatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-my-files-js": () => import("/home/jt/Documents/Gatsby/src/pages/my-files.js" /* webpackChunkName: "component---src-pages-my-files-js" */)
 }
 
