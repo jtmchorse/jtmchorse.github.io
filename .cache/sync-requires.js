f5c62bbdfa9c34b3be6bd3d9e5359c501be37510
JT McHorse <jmchorse@gmail.com>
2019-10-30 06:08:14 -0700

    Day 15, query return and show

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -9,6 +9,7 @@ exports.components = {
   "component---src-pages-about-css-modules-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/about-css-modules.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/index.js"))),
+  "component---src-pages-my-files-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/my-files.js")))
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,42 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div>
+      <h2>Day 15 - Building a page with GraphyQL query</h2>
+      <p>Okay wow, so as a front-ender this is a bit unsual for me.</p>
+      <p>Today we <Link to="my-files">build a page</Link> that uses the query we were just playing around with. Or really, a similar query that actually shows something worth showing.</p>
+      <p>Per usual, we start with a new page, import all the things at the top. Then we get to do one of my forced favorite things. Console log!</p>
+      <img
+        src={"../../Screenshot from 2019-10-30 05-44-54.png"}
+        alt="example code with console.log"
+      />
+      <p>Then we need to actually plop that query into our page. Remember folks, backtick with care `</p>
+      <img
+        src={"../../Screenshot from 2019-10-30 05-48-32.png"}
+        alt="showing graphiql query side by side with query in gatsby"
+      />
+      <p>We can nearly copy paste the query over from our GraphiQL explorer into our new page. In this instance we need to simplify it by dropping the "MyQuery" part. I bet having that exter name will come in handy in the future with multiple queries on a page.</p>
+      <p>Now that we've typed that all in, save that sucker and check your terminal. If you are like me, go ahead and fix all the typos, then go check your browser console.</p>
+      <img
+        src={"../../Screenshot from 2019-10-30 05-53-16.png"}
+        alt="browser console log of query"
+      />
+      <p>Yay, now we know we didn't mess it up. Now that we have the data and know it, we an display it. Continuing to follow along, we build out a table, and then we get to the nitty gritty of accessing said data</p>
+      <p>Again as a basic front-ender I'm not currently 100% on what we're doing here.</p>
+      <img
+        src={"../../Screenshot from 2019-10-30 05-57-23.png"}
+        alt="data access code"
+      />
+      <p>But it does give us the values we want to print out, and in my experience if its valuable we will likey be doing it agian. And again. Eventually it should sink in.</p>
+      <img 
+        src={"../../Screenshot from 2019-10-30 05-59-49.png"}
+        alt="rendered data in the browser"
+      />
+      <p>Tada! we now have a table showing some stuff that we queried. <Link to="my-files">The page</Link>.</p>
+      <p>I'm sure you've noticed a difference in my returned data vs what is shown just in the example just above this <a href="https://www.gatsbyjs.org/tutorial/part-five/#whats-coming-next">link</a></p>
+      <p>My theory is they changed the code snippet in relation to the screen shot. It could also be that this current site isn't a 1 to 1 representation of the example gatsby site in the tutorial. It is likely a mixture of the two, leaning heavily on me not doing it right.</p>
+      <p>The important part though is that we have made our own query, returned it in a new page and displayed it with markup.</p>
+    </div>
+    <div>
       <h2>Day 14 - Data continued, static.</h2>
       <p>There is this thing called a StaticQuery, to be honest it currently feels exactly like the query we did yesterday. We are only grabbing the site title from the gatsby-config.js</p>
       <p>As this is my first real forray into gatsby, it sounds like the static query is a shiny new. Up until this only pages were allowed to do querying?</p>
@@ -18,23 +54,23 @@ const IndexPage = () => (
         <Link to="contact">/contact</Link>
       </p>
       <p>Just in case that gets updated yet again, here's a screen shot of it.</p>
-      <img 
-      src={'../../Screenshot from 2019-10-29 05-36-20.png'}
-      alt="updated contact page header, using static query"
+      <img
+        src={'../../Screenshot from 2019-10-29 05-36-20.png'}
+        alt="updated contact page header, using static query"
       />
       <h2>Bonus - Source Plugins</h2>
       <p>Since the static thing took less time than expected, its time to dive into <a href="https://www.gatsbyjs.org/tutorial/part-five/">part-five</a>.</p>
       <p>Nice, over halfway through these!</p>
       <p>Finally, we're starting to dig into the whole localhost:8000/__graphql setup.</p>
-      <img 
-      src={'../../Screenshot from 2019-10-29 05-54-30.png'}
-      alt="basic graphql query for site metadata, using graphiql"
+      <img
+        src={'../../Screenshot from 2019-10-29 05-54-30.png'}
+        alt="basic graphql query for site metadata, using graphiql"
       />
       <p>Not that I understand <i>just</i> how useful this is going to be, but I can tell for certain that I'd not like to try and sort out those data bindings in my head, if that is what they are called.</p>
       <p>Lol, lets try to make bad things..</p>
-      <img 
-      src={`../../Screenshot from 2019-10-29 06-14-10.png`}
-      alt="bad graphql queries"
+      <img
+        src={`../../Screenshot from 2019-10-29 06-14-10.png`}
+        alt="bad graphql queries"
       />
       <p>Yo dawg, I heard you like children so we childrend your children with some children children children..</p>
       <p>Okay maybe not <i>userful</i> per se, but fun to mess about with and see what you can do.</p>
