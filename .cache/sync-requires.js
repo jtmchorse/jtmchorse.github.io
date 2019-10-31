5afbb57e057fcdcad498fa5e3dfd8994dbdac076
JT McHorse <jmchorse@gmail.com>
2019-10-31 06:17:15 -0700

    squash merged day-16 in

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-about-css-modules-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/about-css-modules.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/contact.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/index.js"))),
-  "component---src-pages-my-files-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/my-files.js")))
+  "component---src-pages-my-files-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/my-files.js"))),
+  "component---src-pages-new-index-js": hot(preferDefault(require("/home/jt/Documents/Gatsby/src/pages/new-index.js")))
 }
 
---
+++ public/styles-6663db5f81c978400c32.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{208:function(e,s,o){},218:function(e,s,o){e.exports={user:"about-css-modules-module--user--2CXbd",avatar:"about-css-modules-module--avatar--2lRF7",description:"about-css-modules-module--description--ev5yS",username:"about-css-modules-module--username--2EBkm",excerpt:"about-css-modules-module--excerpt--2Itwq"}}}]);
+//# sourceMappingURL=styles-6663db5f81c978400c32.js.map
